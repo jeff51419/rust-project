41e81b2f453bd50efe5cbb6d5951f76cfb153628
jeff51419 <jeff51419@gmail.com>
2022-12-02 18:23:12 +0800

    update

--- guessing_game/target/debug/deps/guessing_game-6eec82ec33d1ac5b.d
+++
@@ -1,5 +0,0 @@
-/Users/jeff.yen/code/rust-project/guessing_game/target/debug/deps/guessing_game-6eec82ec33d1ac5b: src/main.rs
-
-/Users/jeff.yen/code/rust-project/guessing_game/target/debug/deps/guessing_game-6eec82ec33d1ac5b.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/deps/guessing_game-cd8345e43ccb20ae.d
+++
@@ -1,5 +0,0 @@
-/Users/jeff.yen/code/rust-project/guessing_game/target/debug/deps/guessing_game-cd8345e43ccb20ae.rmeta: src/main.rs
-
-/Users/jeff.yen/code/rust-project/guessing_game/target/debug/deps/guessing_game-cd8345e43ccb20ae.d: src/main.rs
-
-src/main.rs:
