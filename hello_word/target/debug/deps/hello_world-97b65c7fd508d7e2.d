ae9269c1e75de26020f00b0c82f01af2ece534de
jeff51419 <jeff51419@gmail.com>
2022-12-02 17:05:21 +0800

    rust hello word

---
+++ hello_word/target/debug/deps/hello_world-97b65c7fd508d7e2.d
@@ -0,0 +1,5 @@
+/Users/jeff.yen/code/rust-project/hello_word/target/debug/deps/hello_world-97b65c7fd508d7e2: src/main.rs
+
+/Users/jeff.yen/code/rust-project/hello_word/target/debug/deps/hello_world-97b65c7fd508d7e2.d: src/main.rs
+
+src/main.rs:
---
+++ hello_word/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/jeff.yen/code/rust-project/hello_word/target/debug/hello_world: /Users/jeff.yen/code/rust-project/hello_word/src/main.rs
